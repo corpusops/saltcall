714e03774130f93d43c9d9a08c18667062a7c2bf
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-04-10 11:26:13 +0200

    refresh saltcaller

---
+++ hacking/gen_ansible_saltcaller.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# generate python tags to be imported from inside and  outside the VM (from the hosts)
+# main utility is to generate vim tags afterwards importing the .env in the shell
+import os, sys, re
+CWD = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+import pdb;pdb.set_trace()  ## Breakpoint ##
+path = 'hacking/saltcaller.py'
+sc = open(path).read()
+content = open('library/saltcall.py.in').read()
+sanitizer = re.compile('^#.*', flags=re.M)
+sc = sanitizer.sub('', sc).encode('base64')
+content = content.replace(
+    'SALTCALLER = """',
+    'SALTCALLER = """\n{0}'.format(sc))
+with open('library/saltcall.py', 'w') as d:
+    d.write(content)
+    os.chmod(path, 0o755)
